0105d926e77c116f8e237af06c4c970307153af3
qs33ds-alt <qs33ds@gmail.com>
2026-09-14 00:42:27 +0200

    QS33DS Portal Educativo PWA inicial

---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE = 'qs33ds-edu-v1';
+
+const CORE = [
+  './',
+  './index.html',
+  './laboratori_calcul.html',
+  './lecto_ee.html',
+  './observapt.html',
+  './pictogrames.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(CORE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() =>
+          caches.match(event.request).then(r => r || caches.match('./index.html'))
+        )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const network = fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || network;
+    })
+  );
+});
